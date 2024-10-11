9be155e3c9cf69180d0555aec31abfb8f316e451
mahmoudnagi22 <mahmoud0nagi@gmail.com>
2024-10-11 04:00:24 +0300

    + tasbeh tab

--- lib/core/utils/stringes_manager.dart
+++ lib/core/utils/stringes_manager.dart
@@ -6,6 +6,7 @@ class StringesManager {
   static const String hadithLabel = "Hadith";
   static const String tasbehLabel = "Tasbeh";
   static const String settingLabel = "Setting";
+  static const String tasbehcounter = "عدد التسبيحات ";
 
   static const String ChapterName = "Chapter Name";
   static const String VersesOfNumber = "Verses Number";
